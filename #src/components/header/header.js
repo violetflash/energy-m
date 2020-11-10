3798c0d6c7d4f7891072983ef80fce10e0a29b95
Alex Grobovsky <ktotaam@mail.ru>
2020-11-11 01:08:32 +0500

    header full refactoring

--- #src/components/header/header.js
+++ #src/components/header/header.js
@@ -1,57 +1,10 @@
-///////
-// mega-menu open
-///////
+let buttons_openSearch = document.querySelectorAll('.top-panel__search-btn');
+let forms_SearchForm = document.querySelectorAll('.top-panel__form');
 
-let megaBtn = document.querySelector('.menu__btn');
-let mega_menu = document.querySelector('.mega-menu');
-let menu = document.querySelector('.menu');
-
-megaBtn.addEventListener('click', function(e) {
-    mega_menu.classList.toggle('opened');
-});
-
-
-// megaBtn.addEventListener("mouseenter", function (e) {
-//     mega_menu.classList.add('opened');
-// });
-
-// Hide menu on mouseleave
-// menu.addEventListener("mouseleave", function (e) {
-//     mega_menu.classList.remove('opened');
-// });
-
-// TODO сделать закрытие меню по клику снаружи
-
-//работает с багами
-// window.addEventListener('click', function(e) {
-//
-//     var evTarget = e.target;
-//
-//     if((evTarget !== mega_menu) && (mega_menu.classList.contains('opened')) && (evTarget !== megaBtn) && (evTarget.parentNode !== megaBtn)) {
-//         mega_menu.classList.remove('opened');
-//     }
-//
-// });
-
-//работает с багами
-// window.addEventListener('mouseup', function(event){
-//     if (event.target != mega_menu && event.target != megaBtn && event.target.parentNode != mega_menu){
-//         mega_menu.classList.remove('opened');
-//     }
-// });
-
-//////
-//
-//////
-
-//menu-dropdown
-let dropdownBtn = document.querySelector('.has-dropdown');
-let dropdownBody = dropdownBtn.querySelector('.dropdown-body');
-
-dropdownBtn.addEventListener('mouseenter', function(e) {
-    e.preventDefault()
-    dropdownBody.classList.add('js-visible');
-});
-dropdownBtn.addEventListener('mouseleave', function(e) {
-    dropdownBody.classList.remove('js-visible');
-});
+for (let i = 0; i < buttons_openSearch.length; i++) {
+    const btn_openSearch = buttons_openSearch[i];
+    btn_openSearch.addEventListener('click', function(e) {
+        this.classList.toggle("js-active");
+        forms_SearchForm[i].classList.toggle("js-opened");
+    });
+}
---
+++ #src/components/menu/menu.js
@@ -0,0 +1,62 @@
+///////
+// mega-menu open
+///////
+
+let megaBtn = document.querySelector('.menu__btn');
+let mega_menu = document.querySelector('.mega-menu');
+let menu = document.querySelector('.menu');
+
+megaBtn.addEventListener('click', function(e) {
+    mega_menu.classList.toggle('opened');
+});
+
+
+// megaBtn.addEventListener("mouseenter", function (e) {
+//     mega_menu.classList.add('opened');
+// });
+
+// Hide menu on mouseleave
+// menu.addEventListener("mouseleave", function (e) {
+//     mega_menu.classList.remove('opened');
+// });
+
+// TODO сделать закрытие меню по клику снаружи
+
+//работает с багами
+// window.addEventListener('click', function(e) {
+//
+//     var evTarget = e.target;
+//
+//     if((evTarget !== mega_menu) && (mega_menu.classList.contains('opened')) && (evTarget !== megaBtn) && (evTarget.parentNode !== megaBtn)) {
+//         mega_menu.classList.remove('opened');
+//     }
+//
+// });
+
+//работает с багами
+// window.addEventListener('mouseup', function(event){
+//     if (event.target != mega_menu && event.target != megaBtn && event.target.parentNode != mega_menu){
+//         mega_menu.classList.remove('opened');
+//     }
+// });
+
+//////
+//
+//////
+
+//menu-dropdown
+let dropdownBtn = document.querySelector('.has-dropdown');
+let dropdownBody = dropdownBtn.querySelector('.dropdown-body');
+
+dropdownBtn.addEventListener('click', function(e) {
+    // e.preventDefault()
+    dropdownBody.classList.toggle('js-visible');
+});
+
+// dropdownBtn.addEventListener('mouseenter', function(e) {
+//     e.preventDefault()
+//     dropdownBody.classList.add('js-visible');
+// });
+// dropdownBtn.addEventListener('mouseleave', function(e) {
+//     dropdownBody.classList.remove('js-visible');
+// });
