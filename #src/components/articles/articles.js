42618a0586bef53a419d527aba9d13b34307e4e6
Alex Grobovsky <ktotaam@mail.ru>
2020-11-23 12:03:06 +0500

    added virtual section

--- #src/components/articles/articles.js
+++ #src/components/articles/articles.js
@@ -1,4 +1,4 @@
-let articles = document.querySelectorAll('.articles__figure');
+let articles = document.querySelectorAll('.articles .article-card__figure');
 
 for (let i = 0; i < articles.length; i++) {
     articles[i].addEventListener('mouseleave', function(e) {
